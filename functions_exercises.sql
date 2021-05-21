66461f72847453c925a320f89fe7d30ddfd11631
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-05-21 09:50:24 -0500

    Feat:mysql updates

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -8,19 +8,21 @@ USE employees;
 -- ORDER BY last_name DESC, first_name DESC;
 
 SELECT CONCAT(first_name,' ', last_name) AS full_name FROM employees
-WHERE last_name LIKE 'e%'
-  AND last_name LIKE '%e'
+WHERE last_name like 'e%'
+and last_name LIKE '%e'
 ORDER BY emp_no DESC;
 
 -- SELECT CONCAT(first_name,' ', last_name) AS full_name FROM employees
 -- WHERE last_name LIKE 'e%e';
 
-SELECT CONCAT(first_name, last_name) AS 'Employee', hire_date, birth_date, datediff(now(), hire_date) AS "Days Worked"
+
+SELECT CONCAT(first_name, last_name) AS 'Employee', hire_date,
+       birth_date, dateiff(now(), hire_date) AS "Days Worked"
 FROM employees
 WHERE birth_date LIKE '%%%%-12-25'
-  AND hire_date > '1990-01-01'
+AND hire_date > '1990-01-01'
 ORDER BY birth_date, hire_date DESC;
 
--- SELECT first_name, last_name FROM employees
--- WHERE last_name LIKE '%q%'
+--  SELECT first_name, last_name FROM employees
+--  WHERE last_name LIKE '%q%'
 --   AND last_name NOT LIKE '%qu%';
